0d7ed32f82d598eaf26aaa774aa1f56b4a99de54
Jacob P Wang <59838504+jacobwang7@users.noreply.github.com>
2022-07-16 02:52:22 -0400

    Plant animation

---
+++ script.js
@@ -0,0 +1,33 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+//Set the width & height of the canvas to match the viewport dimensions
+const CANVAS_WIDTH = canvas.width = window.innerWidth;
+const CANVAS_HEIGHT = canvas.height = window.innerHeight;
+//Get access to the sprite sheet
+const saplingSpriteSheet = new Image();
+saplingSpriteSheet.src = "saplingspritesheet.png";
+//saplingSpriteSheet.onload = loadImages;
+console.log(saplingSpriteSheet.width);
+
+let spriteWidth = 100;
+let spriteHeight = 100;
+
+let srcX = 0;
+let srcY = 0;
+
+let frame = 0;
+let framesDrawn = 10;
+
+function animate() {
+    ctx.drawImage(saplingSpriteSheet, srcX * spriteWidth, 0, 100, 100, CANVAS_WIDTH/2 - 50, CANVAS_HEIGHT/2 - 50, 100, 100);
+    if (frame % framesDrawn == 0) {
+        if (srcX < 4) {
+            srcX++;
+        } else {
+            srcX = 0;
+        } 
+    }
+    frame++;
+    requestAnimationFrame(animate);
+};
+animate();
